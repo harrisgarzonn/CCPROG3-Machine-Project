aca7cfd0fb83bdee51b04221615dea59acb0b175
Bien Manoos <manoosgabriel@gmail.com>
2025-10-22 18:12:33 +0800

    Delete Table.java

--- Table.java
+++
@@ -1,9 +0,0 @@
-// Creates a table that will contain 4 products
-public class Table extends Display {
-
-    // Creates a table adding to display
-    public Table(String floor, String grouping, int number) {
-        super(floor, grouping, number, "Table", 4);
-        this.allowedProductTypes = new String[]{"Fruit"};
-    }
-}
