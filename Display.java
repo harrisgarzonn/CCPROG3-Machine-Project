69b49e3fa07450c9edceac9e0779150d233f8ba1
harrisgarzonn <harris_martin_garzon@dlsu.edu.ph>
2025-10-24 00:41:42 +0800

    Update Display.java

--- Display.java
+++ Display.java
@@ -1,5 +1,30 @@
-public class Display{
-
-
-
+public abstract class Display {
+    protected String address;
+    protected int capacity;
+    protected String[] allowedTypes;
+    
+    public Display(String address, int capacity, String[] allowedTypes) {
+        this.address = address;
+        this.capacity = capacity;
+        this.allowedTypes = allowedTypes;
+    }
+    
+    public abstract boolean addProduct(Product product, int slot);
+    public abstract Product removeProduct(int slot);
+    public abstract Product getProduct(int slot);
+    public abstract int getAvailableSlots();
+    public abstract boolean isFull();
+    
+    public boolean canHoldProduct(Product product) {
+        for (String type : allowedTypes) {
+            if (product.getType().equals(type)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public String getAddress() { return address; }
+    public int getCapacity() { return capacity; }
+    public String[] getAllowedTypes() { return allowedTypes; }
 }
