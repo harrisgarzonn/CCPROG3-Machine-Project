38c19b5680b010cd819bcaf237639445ede80b6a
harrisgarzonn <harris_martin_garzon@dlsu.edu.ph>
2025-10-24 00:30:41 +0800

    Update Basket.java

--- Basket.java
+++ Basket.java
@@ -1,12 +1,5 @@
 public class Basket extends Equipment {
-    private static final int BASKET_CAPACITY = 15;
-    
     public Basket() {
-        super(BASKET_CAPACITY);
-    }
-    
-    @Override
-    public String toString() {
-        return String.format("Basket [%d/%d items]", getProductCount(), BASKET_CAPACITY);
+        super(15);
     }
 }
