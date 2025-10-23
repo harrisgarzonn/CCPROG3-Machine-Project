5cc4eadf92da9bc45c8d1c458730c06380033e93
harrisgarzonn <harris_martin_garzon@dlsu.edu.ph>
2025-10-24 00:31:05 +0800

    Update Cart.java

--- Cart.java
+++ Cart.java
@@ -1,12 +1,5 @@
 public class Cart extends Equipment {
-    private static final int CART_CAPACITY = 30;
-    
     public Cart() {
-        super(CART_CAPACITY);
-    }
-    
-    @Override
-    public String toString() {
-        return String.format("Cart [%d/%d items]", getProductCount(), CART_CAPACITY);
+        super(30);
     }
 }
