daa65ecdee18eac310c7f04e18f501b694384d00
harrisgarzonn <harris_martin_garzon@dlsu.edu.ph>
2025-10-24 00:38:15 +0800

    Update Service.java

--- Service.java
+++ Service.java
@@ -1,4 +1,16 @@
-public class Service{
-
-
+public abstract class Service {
+    protected String type;
+    protected int x, y;
+    
+    public Service(String type, int x, int y) {
+        this.type = type;
+        this.x = x;
+        this.y = y;
+    }
+    
+    public abstract String interact(Shopper shopper);
+    
+    public String getType() { return type; }
+    public int getX() { return x; }
+    public int getY() { return y; }
 }
