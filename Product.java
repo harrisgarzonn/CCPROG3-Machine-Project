881a394c4f5313036e7cc8f818301da1e7abcfb0
Bien Manoos <manoosgabriel@gmail.com>
2025-11-25 18:46:13 +0800

    Refactor Product class to include more food types

--- Product.java
+++ Product.java
@@ -1,6 +1,6 @@
 /*
  * This class makes / identify a product or creates a product according to its serialNumber, name, price, type (type of product)
-*/
+ */
 public class Product {
     private String serialNumber;
     private String name;
@@ -34,36 +34,38 @@ public class Product {
 
     // Check if product is food
     public boolean isFood() {
-        if (type.equals("FRUIT") || type.equals("CHICKEN") || type.equals("BEEF") || 
-            type.equals("SEAFOOD") || type.equals("CEREAL") || type.equals("NOODLES") || 
-            type.equals("SNACKS") || type.equals("CANNED_GOODS") || type.equals("CONDIMENTS")) {
-            return true;
-        }
-        return false;
+        return type.equals("FRUIT") || type.equals("VEGETABLE") || type.equals("CHICKEN") ||
+                type.equals("BEEF") || type.equals("SEAFOOD") || type.equals("BREAD") ||
+                type.equals("CEREAL") || type.equals("NOODLES") || type.equals("SNACKS") ||
+                type.equals("CANNED_GOODS") || type.equals("CONDIMENTS") || type.equals("EGGS") ||
+                type.equals("CHEESE") || type.equals("FROZEN_FOOD");
     }
 
     // Check if product is beverage
     public boolean isBeverage() {
-        if (type.equals("SOFT_DRINK") || type.equals("JUICE") || type.equals("ALCOHOL")) {
-            return true;
-        }
-        return false;
+        return type.equals("SOFT_DRINK") || type.equals("JUICE") || type.equals("ALCOHOL") ||
+                type.equals("MILK");
     }
 
     // Check if product can be eaten or drunk
     public boolean isConsumable() {
-        if (isFood() == true || isBeverage() == true) {
-            return true;
-        }
-        return false;
+        return isFood() || isBeverage();
     }
 
     // Get what type of display this product needs
     public String getDisplayType() {
         if (type.equals("FRUIT")) {
             return "TABLE";
+        } else if (type.equals("VEGETABLE")) {
+            return "TABLE";
+        } else if (type.equals("BREAD")) {
+            return "TABLE";
+        } else if (type.equals("EGGS")) {
+            return "TABLE";
         } else if (type.equals("CHICKEN") || type.equals("BEEF") || type.equals("SEAFOOD")) {
             return "CHILLED_COUNTER";
+        } else if (type.equals("MILK") || type.equals("FROZEN_FOOD") || type.equals("CHEESE")) {
+            return "REFRIGERATOR";
         } else {
             return "SHELF";
         }
