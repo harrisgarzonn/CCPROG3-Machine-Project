90852728e5c28e0a43a0fa58b1f8b403e3dba55e
Bien Manoos <manoosgabriel@gmail.com>
2025-11-26 19:48:01 +0800

    Improve documentation for Basket class
    Updated class documentation for clarity and added author/version information.

--- Basket.java
+++ Basket.java
@@ -1,9 +1,15 @@
-/*
-  Basket equipment that can hold up to 15 products.
-  Used for smaller shopping trips.
+/**
+ * Represents a basket equipment that shoppers can use to carry products.
+ * A basket has a fixed capacity of 15 products.
+ *
+ * @author Bien Gabriel L. Manoos, Harris Martin Garzon
+ * @version 2.0
  */
 public class Basket extends Equipment {
+    /**
+     * Constructs a new Basket with a capacity of 15 products.
+     */
     public Basket() {
-        super(15); // Basket can hold 15 products
+        super(15);
     }
 }
