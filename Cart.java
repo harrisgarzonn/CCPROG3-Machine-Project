33a6bab10e0b2d7ae4f74ad1e4dbeb62673ce344
harrisgarzonn <harris_martin_garzon@dlsu.edu.ph>
2025-11-26 18:40:47 +0800

    Update Cart.java

--- Cart.java
+++ Cart.java
@@ -1,5 +1,9 @@
+/*
+  Cart equipment that can hold up to 30 products.
+  Used for larger shopping trips.
+ */
 public class Cart extends Equipment {
     public Cart() {
-        super(30);
+        super(30); // Cart can hold 30 products
     }
 }
