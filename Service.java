436d91982bbb1244d76ea76333888b44ca4ca817
harrisgarzonn <harris_martin_garzon@dlsu.edu.ph>
2025-11-26 18:50:43 +0800

    Update Service.java

--- Service.java
+++ Service.java
@@ -1,7 +1,11 @@
+/*
+  Abstract base class for all service amenities in the supermarket.
+  Services are interactive facilities that shoppers can use.
+ */
 public abstract class Service {
-    protected String type;
-    protected int x, y;
-    protected String floor;
+    protected String type; // Type of service (e.g., "ENTRANCE", "CHECKOUT")
+    protected int x, y; // Position coordinates on the map
+    protected String floor; // Which floor the service is on ("GF" or "2F")
 
     public Service(String type, int x, int y, String floor) {
         this.type = type;
@@ -10,8 +14,10 @@ public abstract class Service {
         this.floor = floor;
     }
 
+    // Abstract method for service interaction
     public abstract String interact(Shopper shopper);
 
+    // Getters
     public String getType() { return type; }
     public int getX() { return x; }
     public int getY() { return y; }
