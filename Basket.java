c1d7478666ea4c6105b1548ac5148399561121c1
harrisgarzonn <harris_martin_garzon@dlsu.edu.ph>
2025-11-26 18:24:05 +0800

    Update Basket.java

--- Basket.java
+++ Basket.java
@@ -1,5 +1,9 @@
+/*
+  Basket equipment that can hold up to 15 products.
+  Used for smaller shopping trips.
+ */
 public class Basket extends Equipment {
     public Basket() {
-        super(15);
+        super(15); // Basket can hold 15 products
     }
 }
